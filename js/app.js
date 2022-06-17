6ae94bbe79af814d9e2c1d92c55945cf44cc3e0f
efontecha <kitservi@hotmail.com>
2022-06-16 21:52:53 -0500

    Entrega eventos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let nombre="Edgar"
-let apellido="Pirachican"
-console.log(nombre+"-"+apellido)
+let nombre = "Edgar"
+let apellido = "Pirachican"
+console.log(nombre + "-" + apellido)
 console.log("Desafio array y objectos")
 
 const regex = /^[0-9]*$/;
@@ -199,28 +199,29 @@ const regex = /^[0-9]*$/;
 //combinacion array objectos
 //const jsonData= require('./camisa.json'); 
 
-class camisa{
-        constructor(codigo,talla,precio,moneda,stock,imagen,promocion){
-            this.codigo=codigo;    
-            this.talla= talla;
-            this.precio=precio;
-            this.moneda=moneda;
-            this.stock=stock;
-            this.imagen= imagen;
-            this.promocion=promocion
-        }
-        stocactual(){
-            console.log("Stock:"+camisa.stock)
-        } }
+class camisa {
+    constructor(codigo, talla, precio, moneda, stock, imagen, promocion) {
+        this.codigo = codigo;
+        this.talla = talla;
+        this.precio = precio;
+        this.moneda = moneda;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.promocion = promocion
+    }
+    stocactual() {
+        console.log("Stock:" + camisa.stock)
+    }
+}
 
-const camisa01 = new camisa("C0001",32,18,"USD",10,"./imagen/camisa01.png","N")
-const camisa02 = new camisa("C0002",34,20,"USD",28,"./imagen/camisa02.png","N")
-const camisa03 = new camisa("C0003",36,22,"USD",5,"./imagen/camisa03.png","N")
-const camisa04 = new camisa("C0004",32,18,"USD",3,"./imagen/camisa04.png","S")
-const camisa05 = new camisa("C0005",34,20,"USD",23,"./imagen/camisa05.png","S")
-const camisa06 = new camisa("C0006",36,22,"USD",31,"./imagen/camisa06.png","S")
-                        
-const coleccion =[camisa01,camisa02,camisa03,camisa04,camisa05,camisa06]         
+const camisa01 = new camisa("C0001", 32, 18, "USD", 10, "./imagen/camisa01.png", "N")
+const camisa02 = new camisa("C0002", 34, 20, "USD", 28, "./imagen/camisa02.png", "N")
+const camisa03 = new camisa("C0003", 36, 22, "USD", 5, "./imagen/camisa03.png", "N")
+const camisa04 = new camisa("C0004", 32, 18, "USD", 3, "./imagen/camisa04.png", "S")
+const camisa05 = new camisa("C0005", 34, 20, "USD", 23, "./imagen/camisa05.png", "S")
+const camisa06 = new camisa("C0006", 36, 22, "USD", 31, "./imagen/camisa06.png", "S")
+
+const coleccion = [camisa01, camisa02, camisa03, camisa04, camisa05, camisa06]
 
 const cardContainer = document.getElementById('cardContainer')
 
@@ -241,67 +242,83 @@ coleccion.forEach((coleccion) => {
 
 const collection = document.getElementsByClassName("cardPrice");
 for (let i = 0; i < collection.length; i++) {
-  collection[i].style.backgroundColor = "gray";
-  collection[i].style.color= "white";
+    collection[i].style.backgroundColor = "gray";
+    collection[i].style.color = "white";
 }
 
-const miCarrito=[];
+const miCarrito = [];
 
-const totalCarrito = (Arr) =>{
-    let total =0;
-    Arr.forEach((articulo) =>{
+const totalCarrito = (Arr) => {
+    let total = 0;
+    Arr.forEach((articulo) => {
         total += articulo.precio
     })
     let html = document.querySelector('#totalCarrito');
-    html.innerHTML= "USD:"+total.toLocaleString();
-  
-    let numero =0;
-   
+    html.innerHTML = "USD:" + total.toLocaleString();
+
+    let numero = 0;
+
     let htmlItem = document.querySelector('#itemCarrito');
-    htmlItem.innerHTML= Arr.length;
- console.log("Array-------"+Arr.length)
- 
+    htmlItem.innerHTML = Arr.length;
+    console.log("Array-------" + Arr.length)
+
 }
 
-const compra =(totalCarrito) => {
+const compra = (totalCarrito) => {
     console.log("Pintar")
 }
 
-const codigoCamisa =(e)=>{
-  console.log(e.target.getAttribute("id"))
-  const camisaElegida = e.target.getAttribute("id")
-  const buscarElegida = coleccion.find((colecionCamisa) => colecionCamisa.codigo==camisaElegida)
-  miCarrito.push(buscarElegida)
-  totalCarrito(miCarrito)
-  localStorage.getItem("Carrito",miCarrito)
-  sessionStorage.getItem("ariculos",coleccion)
-  console.log(miCarrito)
-  const jsonlocal = JSON.stringify(miCarrito);
-localStorage.setItem("registroVenta",jsonlocal);
-const getlocalStorage = localStorage.getItem(registroVenta)
-console.log(getlocalStorage);
-}
-const botonAgregar = document.querySelectorAll('.button')
+// se ejecuta una vez click botom
+const codigoCamisa = (e) => {
+    console.log(e.target.getAttribute("id"))
+    const camisaElegida = e.target.getAttribute("id")
+    const buscarElegida = coleccion.find((colecionCamisa) => colecionCamisa.codigo == camisaElegida)
+    miCarrito.push(buscarElegida)
+    totalCarrito(miCarrito)
 
-botonAgregar.forEach((agregarCompra) =>{
- agregarCompra.addEventListener('click',codigoCamisa)
+    localStorage.getItem("Carrito", miCarrito)
+    sessionStorage.getItem("ariculos", coleccion)
+    console.log(miCarrito)
+
+    const jsonlocal = JSON.stringify(miCarrito);
+    localStorage.setItem("registroVenta", jsonlocal);
+
+    const getlocalStorage = localStorage.getItem(registroVenta)
+    console.log(getlocalStorage);
+}
+
+//Click sobre el botom
+const botonAgregar = document.querySelectorAll('.button')
+botonAgregar.forEach((agregarCompra) => {
+    agregarCompra.addEventListener('click', codigoCamisa)
 })
 
-localStorage.setItem("Token","abc123");
-sessionStorage.setItem("isDarkMode",true);
+//LocalStorage SessionStorage
+localStorage.setItem("Token", "abc123");
+sessionStorage.setItem("isDarkMode", true);
 
 
 const lstorage = localStorage.getItem("Token");
-const sStorage= sessionStorage.getItem("isDarkMode");
+const sStorage = sessionStorage.getItem("isDarkMode");
 
 console.log(lstorage);
 console.log(sStorage);
 
-for(let i=0; i< localStorage.length; i++){
+for (let i = 0; i < localStorage.length; i++) {
     let key = localStorage.key(i);
-    console.log("Variable:"+key+" valor:"+localStorage.getItem(key))
-
+    console.log("Variable:" + key + " valor:" + localStorage.getItem(key))
 }
 
+//Detalle carrito
+
+const consultaDetalle = () => {
+console.log("Consultar detalle carrito....")
+}
+
+const botonDetalleCarrito = document.querySelector('#btonDetalle')
+botonDetalleCarrito.addEventListener('click', consultaDetalle )
+
+
+//botonDetalleCarrito.addEventListener('click', consultaDetalle)
 
 
